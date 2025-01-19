523040ce30b169eb5d52593a1b2fea7aaef403b0
Anakin-bb8 <samuelebergamasco2011@gmail.com>
2025-01-19 12:24:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById('version').addEventListener('click', function() {
     var platform = getPlatform();
     var downloadLink;
     if (platform === "Windows") {
-        downloadLink = "https://github.com/Anakin-bb8/Nuitka-GUI-NPTE/releases/download/NPTE1.0.5.0/NPTESetup1.0.5.0.exe";
+        downloadLink = "https://github.com/Anakin-bb8/Nuitka-GUI-NPTE/releases/download/NPTE1.0.6.0/NPTESetup1.0.6.0.exe";
     } else if (platform === "MacOS") {
         downloadLink = "https://github.com/Anakin-bb8/Nuitka-GUI-NPTE/releases/download/NPTE1.0.5.0/NPTEInstaller1.1.pkg";
     } else {
